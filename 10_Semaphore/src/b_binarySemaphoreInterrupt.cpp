6680064e5c8424770f13ca365d24134e1f8b4f48
Wilden Ali <wildenwildenaliali@gmail.com>
2021-11-25 10:27:52 +0700

    binary semaphore using interrupt

--- 10_Semaphore/src/b_binarySemaphoreInterrupt.cpp
+++ 10_Semaphore/src/b_binarySemaphoreInterrupt.cpp
@@ -1,70 +1,70 @@
-#include <Arduino.h>
-#include <Arduino_FreeRTOS.h>
-#include "semphr.h"
+// #include <Arduino.h>
+// #include <Arduino_FreeRTOS.h>
+// #include "semphr.h"
 
 
-/*
-https://microcontrollerslab.com/freertos-binary-semaphore-tasks-interrupt-synchronization-u-arduino/
+// /*
+// https://microcontrollerslab.com/freertos-binary-semaphore-tasks-interrupt-synchronization-u-arduino/
 
-*/
+// */
 
 
-#define  LED  13
+// #define  LED  13
 
-SemaphoreHandle_t interruptSemaphore;
+// SemaphoreHandle_t interruptSemaphore;
 
-void TaskLedon(void *pvParameters);
-void TaskLedoff(void *pvParameters);
-void interruptHandler();
+// void TaskLedon(void *pvParameters);
+// void TaskLedoff(void *pvParameters);
+// void interruptHandler();
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED ,OUTPUT);
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(LED ,OUTPUT);
   
-  xTaskCreate(TaskLedon, "LedON", 100, NULL, 1, NULL);
-  xTaskCreate(TaskLedoff, "LedOFF", 100, NULL, 1, NULL);
+//   xTaskCreate(TaskLedon, "LedON", 100, NULL, 1, NULL);
+//   xTaskCreate(TaskLedoff, "LedOFF", 100, NULL, 1, NULL);
 
-  interruptSemaphore = xSemaphoreCreateBinary();
-  if (interruptSemaphore != NULL) 
-  {
-    attachInterrupt(digitalPinToInterrupt(2), interruptHandler, HIGH);
-  }
-}
+//   interruptSemaphore = xSemaphoreCreateBinary();
+//   if (interruptSemaphore != NULL) 
+//   {
+//     attachInterrupt(digitalPinToInterrupt(2), interruptHandler, HIGH);
+//   }
+// }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
 
-void interruptHandler() 
-{ 
-  Serial.println("Semaphore is given");
-  BaseType_t  xHigherPriorityTaskWoken  pdFALSE;
-  xSemaphoreGiveFromISR(interruptSemaphore, &xHigherPriorityTaskWoken);
-}
+// void interruptHandler() 
+// { 
+//   Serial.println("Semaphore is given");
+//   BaseType_t  xHigherPriorityTaskWoken  pdFALSE;
+//   xSemaphoreGiveFromISR(interruptSemaphore, &xHigherPriorityTaskWoken);
+// }
 
-void TaskLedon(void *pvParameters)
-{
-  (void) pvParameters;
+// void TaskLedon(void *pvParameters)
+// {
+//   (void) pvParameters;
 
-  for (;;) 
-    {
-      if (xSemaphoreTake(interruptSemaphore, portMAX_DELAY) == pdPASS) 
-      {
-        Serial.println("TaskLedon Received Semaphore");
-        digitalWrite(LED_BUILTIN, HIGH);
-      }
-    }
-  }
+//   for (;;) 
+//     {
+//       if (xSemaphoreTake(interruptSemaphore, portMAX_DELAY) == pdPASS) 
+//       {
+//         Serial.println("TaskLedon Received Semaphore");
+//         digitalWrite(LED_BUILTIN, HIGH);
+//       }
+//     }
+//   }
 
-void TaskLedoff(void *pvParameters)
-{
-  (void) pvParameters;
-  for (;;) 
-  {
-    if (xSemaphoreTake(interruptSemaphore, portMAX_DELAY) == pdPASS)
-    {
-      Serial.println("TaskLedoff Received Semaphore");
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-  }
-}
+// void TaskLedoff(void *pvParameters)
+// {
+//   (void) pvParameters;
+//   for (;;) 
+//   {
+//     if (xSemaphoreTake(interruptSemaphore, portMAX_DELAY) == pdPASS)
+//     {
+//       Serial.println("TaskLedoff Received Semaphore");
+//       digitalWrite(LED_BUILTIN, LOW);
+//     }
+//   }
+// }
